83790ff3c71e1385a2d6d78631ddac8877f4753b
Arjun cv <arjun7180@gmail.com>
2024-06-07 15:58:07 +0530

    made some bug fixes

--- src/main.jsx
+++ src/main.jsx
@@ -22,7 +22,7 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <Navigate to="/articles" />,
+        element: <Navigate to="articles" />,
       },
       {
         path: "articles",
